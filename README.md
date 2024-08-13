769b884099c4f4551a960521e99f1fcc0979e610
Bhavana <77827697+chbhavana-3@users.noreply.github.com>
2024-08-13 16:39:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,62 @@
+# Chess Piece Detection | OpenCV 
+This project involves loading multiple images of chess boards and utilizing the matchTemplate method of OpenCV to detect the pieces present on each board. As a result of this process, the output includes a comprehensive list of the detected chess piece names.
+
+<p align="center">
+    <img src="dist/board.jpg" />
+    <img src="dist/board (9).jpg" />
+    <img src="dist/board (10).jpg" />
+    <img src="dist/board (1).jpg" />
+</p>
+
+
+|                Piece                 |      Name      | Short Name |                   Piece                    |      Name      | Short Name |
+| :----------------------------------: | :------------: | :--------: | :----------------------------------------: | :------------: | :--------: |
+|  ![King (White)](chess_piece/K.png)  |  King (White)  |     K      |  ![King (black)](chess_piece/k_black.png)  |  King (black)  |     k      |
+| ![Queen (white)](chess_piece/Q.png)  | Queen (White)  |     Q      | ![Queen (black)](chess_piece/q_black.png)  | Queen (black)  |     q      |
+|  ![Rook (White)](chess_piece/R.png)  |  Rook (White)  |     R      |  ![Rook (black)](chess_piece/r_black.png)  |  Rook (black)  |     r      |
+| ![Bishop (white)](chess_piece/B.png) | Bishop (White) |     B      | ![Bishop (black)](chess_piece/b_black.png) | Bishop (black) |     b      |
+| ![Knight (White)](chess_piece/N.png) | Knight (White) |     N      | ![Knight (black)](chess_piece/n_black.png) | Knight (black) |     n      |
+|  ![Pawn (White)](chess_piece/P.png)  |  Pawn (White)  |     P      |  ![Pawn (black)](chess_piece/p_black.png)  |  Pawn (black)  |     p      |
+
+
+
+## Requirements
+
+- Python 3.x
+- OpenCV
+- Numpy
+
+## Usage
+
+To get started, first clone the repository and install the necessary dependencies by running the following command:
+
+```bash
+pip install opencv-python numpy
+```
+
+Next, place the chess board image files in the `test/` directory and the chess piece image files in the `chess_piece/` directory.
+
+There are two ways to execute the program:
+
+**Method 1: Running in Terminal**
+
+To run the script in the terminal, use the following command:
+
+```bash
+python main.py
+```
+
+**Method 2: Running in Jupyter Lab**
+
+To run the script in `Jupyter Lab`, open the `main.ipynb` notebook and execute the code.
+
+## Configuration
+
+The following configurations can be adjusted according to the user's preference:
+
+- `SHOW_IMAGE`: If set to `True`, the image with detected pieces will be displayed. Default is `True`.
+- `EXPORT_IMAGE`: If set to `True`, the image with detected pieces will be saved to the `dist/` directory. Default is `True`.
+- `chessPieceThreshold`: The threshold values for each chess piece can be adjusted according to the image quality. The default values are provided in the script.
+- `CHESS_BOARD_OUTPUT_DIR`: The directory where the output images will be saved. Default is `dist/`.
+
+
---
+++ main.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+import glob
+import os
+import re
+
+CHESS_PIECE_DIR = glob.glob('chess_piece/*.*')
+CHESS_BOARD_DIR = glob.glob('test/*.*')
+CHESS_BOARD_OUTPUT_DIR = os.path.join('dist')
+
+SHOW_IMAGE = True
+EXPORT_IMAGE = True
+
+# The threshold values for each chess piece can be adjusted according to the image quality
+chessPieceThreshold = {
+    'B': 0.2, #bishop
+    'b': 0.3, #bishop_black
+    'K': 0.2, #king
+    'k': 0.2, #king_black
+    'N': 0.1, #knight
+    'n': 0.2, #knight_black
+    'P': 0.15,#pawn
+    'p': 0.3, #pawn_black
+    'Q': 0.2, #queen
+    'q': 0.2, #queen_black
+    'R': 0.2, #rook
+    'r': 0.2, #rook_black
+}
+
+chessPieceImages = dict()
+
+for path in CHESS_PIECE_DIR:
+    baseName = os.path.basename(path)
+    fileName = re.search('[\w() -]+?(?=\.)', baseName).group(0)[0]
+    pieceImage = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    print(pieceImage)
+    chessPieceImages[fileName] = (pieceImage, chessPieceThreshold[fileName])
+    
+chessBoardImages = dict()
+
+for idx, path in enumerate(CHESS_BOARD_DIR):
+    baseName = os.path.basename(path)
+    fileName = re.search('[\w() -]+?(?=\.)', baseName).group(0)
+    boardImage = cv2.imread(path)
+    chessBoardImages[fileName] = boardImage
+
+def detectPieceOfChess(boardName, boardImage):
+    for piece in chessPieceImages:
+        pieceImage = chessPieceImages[piece][0]
+        pieceThreshold = chessPieceImages[piece][1]
+        pieceName = piece
+        
+        boardImageGray = cv2.cvtColor(boardImage, cv2.COLOR_BGR2GRAY)
+        pieceImageGray = cv2.cvtColor(pieceImage, cv2.COLOR_BGR2GRAY)
+        
+        mask = pieceImage[:,:,3]
+        h, w = pieceImageGray.shape
+            
+        result = cv2.matchTemplate(boardImageGray, pieceImageGray, cv2.TM_SQDIFF_NORMED, mask=mask)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        
+        while min_val < pieceThreshold:
+            top_left = min_loc
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            
+            rectangleColor = (0,250,50)
+            cv2.rectangle(boardImage, top_left, bottom_right, rectangleColor, 2)
+                          
+            textColor = (255,0,0) if pieceName.isupper() else (0,0,255) # white piece is blue and black piece is red
+            textPosition =  (top_left[0], top_left[1] + 20)
+            cv2.putText(boardImage, pieceName, textPosition, cv2.FONT_HERSHEY_SIMPLEX, 0.7, textColor, 2, cv2.LINE_AA)
+            
+            # overwrite the portion of the result that has the match:
+            h1 = top_left[1]-h//2
+            h1 = np.clip(h1, 0, result.shape[0])
+
+            h2 = top_left[1] + h//2 + 1
+            h2 = np.clip(h2, 0, result.shape[0])
+
+            w1 = top_left[0] - w//2
+            w1 = np.clip(w1, 0, result.shape[1])
+
+            w2 = top_left[0] + w//2 + 1
+            w2 = np.clip(w2, 0, result.shape[1])
+            
+            # poison the result in the vicinity of this match so it isn't found again
+            result[h1:h2, w1:w2] = 1
+            
+            # look for next match
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            
+    if EXPORT_IMAGE:
+        cv2.imwrite(os.path.join(CHESS_BOARD_OUTPUT_DIR, boardName + '.jpg'), boardImage)
+        
+    if SHOW_IMAGE:
+        cv2.imshow(boardName, boardImage)
+        
+for boardName in chessBoardImages:
+    detectPieceOfChess(boardName, chessBoardImages[boardName])
+    
+cv2.waitKey(0)
+cv2.destroyAllWindows()
